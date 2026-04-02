46c146f6fcfde8ad0971ab7834f13c21a0741c6a
Adidev <anandadidev43@gmail.com>
2026-04-02 22:46:44 +0530

    removing static file dependecy due to conflict

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -192,9 +192,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # optional, if you use custom static
-]
 LOGGING = {
     "version": 1,
     "handlers": {
